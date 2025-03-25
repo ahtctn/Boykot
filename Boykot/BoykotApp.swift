627b9c16cb0c2862894bd69ee06eadef40b0c021
Ahmet Ali Çetin <ahtctn@gmail.com>
2025-03-25 13:33:30 +0300

    Initial Commit

---
+++ Boykot/BoykotApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoykotApp.swift
+//  Boykot
+//
+//  Created by Ahmet Ali ÇETİN on 25.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct BoykotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
